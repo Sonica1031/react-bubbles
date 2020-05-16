2d136748e07160e9da5ab70d31e47537cf59727c
Sonica1031 <Sonica1031@hotmail.com>
2020-05-16 12:21:58 -0500

    login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const onLogin = (e) =>{
+    e.preventDefault();
+    axios
+    .post('http://localhost:5000/api/login', {username: username, password: password})
+    .then(res => {
+      localStorage.setItem("token", res.data.payload)
+      props.history.push("/BubblePage")
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form>
+        <input
+        type="text"
+        placeholder="Enter username"
+        onChange={e => setUsername(e.target.value)}
+        />
+        <input
+        type="password"
+        placeholder="Enter password"
+        onChange={e => setPassword(e.target.value)}
+        />
+        <button onClick={onLogin}>Login</button>
+      </form>
     </>
   );
 };
